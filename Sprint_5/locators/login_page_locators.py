ebd2cdd6ab91340715a11d24d38cd4bb887b5303
Nadia Beznosova <beznosovanadia@yandex.ru>
2025-10-13 09:34:37 +0300

    Откат main

--- Sprint_5/locators/login_page_locators.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class LoginPageLocators:
-    LOGIN_BUTTON_MAIN = (By.XPATH, "//button[text()='Войти в аккаунт']")
-    EMAIL_FIELD = (By.NAME, "email")
-    PASSWORD_FIELD = (By.NAME, "password")
-    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")
--- qa_python_1/task_2_extra.py
+++
@@ -1,5 +0,0 @@
-def digit_root(num):
-    while num > 9:
-        digits = [int(d) for d in str(num)]
-        num = sum(digits)
-    return num
